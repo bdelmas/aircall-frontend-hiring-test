0aa3a37d7e172de041e7edd87b6810a1eab9f194
Benjamin Delmas <bdelmas.pro@gmail.com>
2022-09-09 19:26:10 +0200

    Update axios base URL

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-export const instance = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com",
+export const ax = axios.create({
+  baseURL: "https://frontend-test-api.aircall.dev",
 });
